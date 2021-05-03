746c78fc054115e6228f61304600ad654af50ced
louful <reiberlouisgif@yahoo.fr>
2021-05-04 00:33:08 +0200

    Update of the main with link with kafka and schema of the json object

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql import functions as F
+
 
 
 # setup arguments
@@ -9,17 +13,37 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka
 
 spark = SparkSession.builder.master("local").getOrCreate()
 
+schema = StructType(
+    [
+        StructField('key', StringType(), True),
+        StructField('client_uuid', StringType(), True),
+        StructField('request_dt', StringType(), True),
+        StructField('latitude', StringType(), True),
+        StructField('longitude', StringType(), True)
+
+    ]
+)
 
 df = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "0.0.0.0:9092") \
-  .option("subscribe", "topic1") \
+  .option("subscribe", "taxi") \
   .load()
 
+
+
+
+df_test = df.select(from_json(col("value").cast("string"), schema).alias("value"))\
+    .select(
+    F.col('value').getItem('key'),
+    F.col('value').getItem('client_uuid'),
+    F.col('value').getItem('request_dt'),
+    F.col('value').getItem('latitude'),
+    F.col('value').getItem('longitude'))
+
 # Write key-value data from a DataFrame to a specific Kafka topic specified in an option
-ds = df \
- .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+ds = df_test \
  .writeStream \
  .format("console") \
  .start()\

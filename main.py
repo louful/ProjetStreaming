7e053fa85974f24428e1d88ddf1fc6127d71bf17
Alexis Miguel <AlexisMiguel@pc-5.home>
2021-05-14 22:44:53 +0200

    update of main

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
 import os
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType
-from pyspark.sql import functions as F
+from processing import extract_nested_fields, extract_nested_fields1 ,get_zone
+from schema import get_schema_taxi_request, get_schema_ride
 
 
+def run_stream():
+    # Initialize the stream
+    spark = SparkSession.builder.master("local").getOrCreate()
 
-# setup arguments
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12-3.1.1.jar'
+    # Subscribe to a kafka topic "taxi"
+    df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "0.0.0.0:9092") \
+        .option("subscribe", "taxi") \
+        .load()
 
+    schema = get_schema_taxi_request()
+    schema1 = get_schema_ride()
 
-spark = SparkSession.builder.master("local").getOrCreate()
+# Business logic
+    df_with_unnested_fields = extract_nested_fields(df, schema)
+    df_with_zone = get_zone(df_with_unnested_fields)
 
-schema = StructType(
-    [
-        StructField('key', StringType(), True),
-        StructField('client_uuid', StringType(), True),
-        StructField('request_dt', StringType(), True),
-        StructField('latitude', StringType(), True),
-        StructField('longitude', StringType(), True)
+    # Write key-value data from a DataFrame to a specific Kafka topic specified in an option
+    df_with_zone \
+        .writeStream \
+        .format("console") \
+        .start() \
+        .awaitTermination()
 
-    ]
-)
-
-df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "0.0.0.0:9092") \
-  .option("subscribe", "taxi") \
-  .load()
-
-
-df_test = df.select(from_json(col("value").cast("string"), schema).alias("value")).select(
-    F.col('value').getItem('key'),
-    F.col('value').getItem('client_uuid'),
-    F.col('value').getItem('request_dt'),
-    F.col('value').getItem('latitude'),
-    F.col('value').getItem('longitude')
-)
-
-# Write key-value data from a DataFrame to a specific Kafka topic specified in an option
-ds = df_test \
- .writeStream \
- .format("console") \
- .start()\
- .awaitTermination()
+if __name__ == '__main__':
+    run_stream()
---
+++ processing.py
@@ -0,0 +1,44 @@
+from pyspark.sql.functions import from_json, col, when
+from pyspark.sql import functions as F
+from pyspark.sql.dataframe import DataFrame
+from zone import ZONE_1, ZONE_2, ZONE_3, ZONE_4, ZONE_5, ZONE_6, ZONE_7, ZONE_8, ZONE_9
+
+def extract_nested_fields(dataframe: DataFrame, schema):
+    df_test = dataframe.select(from_json(col("value").cast("string"), schema).alias("value")).select(
+        F.col('value').getItem('key'),
+        F.col('value').getItem('client_uuid'),
+        F.col('value').getItem('request_dt'),
+        F.col('value').getItem('latitude'),
+        F.col('value').getItem('longitude')
+    )
+    return df_test
+
+def extract_nested_fields1(dataframe: DataFrame, schema1):
+    df_test1 = dataframe.select(from_json(col("value").cast("string"), schema1).alias("value")).select(
+        F.col('value').getItem('key'),
+        F.col('value').getItem('amount'),
+        F.col('value').getItem('pickup_time'),
+        F.col('value').getItem('dropoff_time'),
+        F.col('value').getItem('pickup_latitude'),
+        F.col('value').getItem('pickup_longitude'),
+        F.col('value').getItem('dropoff_latitude'),
+        F.col('value').getItem('dropoff_longitude'),
+        F.col('value').getItem('tips')
+    )
+    return df_test1
+
+def get_zone(dataframe: DataFrame):
+
+    dataframe_with_zone = dataframe.withColumn("zone",
+                                               when(col("latitude") < ZONE_1.latitude_1 & col("latitude") > ZONE_1.latitude_2 & col("longitude") < ZONE_1.longitude_1 & col("longitude") > ZONE_1.longitude_2, "ZONE1")
+                                               .when(col("latitude") < ZONE_2.latitude_1 & col("latitude") > ZONE_2.latitude_2 & col("longitude") < ZONE_2.longitude_1 & col("longitude") > ZONE_2.longitude_2, "ZONE2")
+                                               .when(col("latitude") < ZONE_3.latitude_1 & col("latitude") > ZONE_3.latitude_2 & col("longitude") < ZONE_3.longitude_1 & col("longitude") > ZONE_3.longitude_2, "ZONE3")
+                                               .when(col("latitude") < ZONE_4.latitude_1 & col("latitude") > ZONE_4.latitude_2 & col("longitude") < ZONE_4.longitude_1 & col("longitude") > ZONE_4.longitude_2, "ZONE4")
+                                               .when(col("latitude") < ZONE_5.latitude_1 & col("latitude") > ZONE_5.latitude_2 & col("longitude") < ZONE_5.longitude_1 & col("longitude") > ZONE_5.longitude_2, "ZONE5")
+                                               .when(col("latitude") < ZONE_6.latitude_1 & col("latitude") > ZONE_6.latitude_2 & col("longitude") < ZONE_6.longitude_1 & col("longitude") > ZONE_6.longitude_2, "ZONE6")
+                                               .when(col("latitude") < ZONE_7.latitude_1 & col("latitude") > ZONE_7.latitude_2 & col("longitude") < ZONE_7.longitude_1 & col("longitude") > ZONE_7.longitude_2, "ZONE7")
+                                               .when(col("latitude") < ZONE_8.latitude_1 & col("latitude") > ZONE_8.latitude_2 & col("longitude") < ZONE_8.longitude_1 & col("longitude") > ZONE_8.longitude_2, "ZONE8")
+                                               .when(col("latitude") < ZONE_9.latitude_1 & col("latitude") > ZONE_9.latitude_2 & col("longitude") < ZONE_9.longitude_1 & col("longitude") > ZONE_9.longitude_2, "ZONE9")
+                                               )
+
+    return dataframe_with_zone

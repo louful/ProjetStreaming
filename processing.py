24f3b8dc6b7f3d9f15722d9b79479044d606a274
Alexis Miguel <AlexisMiguel@pc-5.home>
2021-05-16 00:29:16 +0200

    processing with zone

--- processing.py
+++ processing.py
@@ -9,8 +9,7 @@ def extract_fields_demands(dataframe: DataFrame, schema):
         F.col('value').getItem('client_uuid'),
         F.col('value').getItem('request_dt'),
         F.col('value').getItem('latitude').alias("latitude"),
-
-        F.col('value').getItem('longitude')
+        F.col('value').getItem('longitude').alias("longitude")
     )
     return df_test
 
@@ -31,8 +30,15 @@ def extract_fields_taxi_ride(dataframe: DataFrame, schema1):
 def get_zone(dataframe: DataFrame):
 
     dataframe_with_zone = dataframe.withColumn("zone",
-                                               when((col("latitude") < ZONE_1.latitude_1), "ZONE1" )
-
-                                            )
+                                               when((col("latitude") < ZONE_1.latitude_1) & col(("latitude") > ZONE_1.latitude_2) & col(("longitude") > ZONE_1.longitude_1) & col(("longitude") < ZONE_1.longitude_2), "ZONE1")
+                                               .when((col("latitude") < ZONE_2.latitude_1) & col(("latitude") > ZONE_2.latitude_2) & col(("longitude") > ZONE_2.longitude_1) & col(("longitude") < ZONE_2.longitude_2), "ZONE2")
+                                               .when((col("latitude") < ZONE_3.latitude_1) & col(("latitude") > ZONE_3.latitude_2) & col(("longitude") > ZONE_3.longitude_1) & col(("longitude") < ZONE_3.longitude_2), "ZONE3")
+                                               .when((col("latitude") < ZONE_4.latitude_1) & col(("latitude") > ZONE_4.latitude_2) & col(("longitude") > ZONE_4.longitude_1) & col(("longitude") < ZONE_4.longitude_2), "ZONE4")
+                                               .when((col("latitude") < ZONE_5.latitude_1) & col(("latitude") > ZONE_5.latitude_2) & col(("longitude") > ZONE_5.longitude_1) & col(("longitude") < ZONE_5.longitude_2), "ZONE5")
+                                               .when((col("latitude") < ZONE_6.latitude_1) & col(("latitude") > ZONE_6.latitude_2) & col(("longitude") > ZONE_6.longitude_1) & col(("longitude") < ZONE_6.longitude_2), "ZONE6")
+                                               .when((col("latitude") < ZONE_7.latitude_1) & col(("latitude") > ZONE_7.latitude_2) & col(("longitude") > ZONE_7.longitude_1) & col(("longitude") < ZONE_7.longitude_2), "ZONE7")
+                                               .when((col("latitude") < ZONE_8.latitude_1) & col(("latitude") > ZONE_8.latitude_2) & col(("longitude") > ZONE_8.longitude_1) & col(("longitude") < ZONE_8.longitude_2), "ZONE8")
+                                               .when((col("latitude") < ZONE_9.latitude_1) & col(("latitude") > ZONE_9.latitude_2) & col(("longitude") > ZONE_9.longitude_1) & col(("longitude") < ZONE_9.longitude_2), "ZONE9")
+                                               )
 
     return dataframe_with_zone
